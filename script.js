3eea94e062d0cf8c0bce8de5094fe0e73b4fac4c
AndreyGOIT <ae.borga@gmail.com>
2025-01-21 11:19:58 +0200

    add tuotteet table

--- script.js
+++ script.js
@@ -55,13 +55,36 @@ const tuotteet = [
     const kate = ((myyntihinta - ostohinta) / ostohinta) * 100;
     return kate;
   }
+  //loop joka laskee kate ja lisaa kate tuotteeseen
   for (let i = 0; i < tuotteet.length; i++) {
     const tuote = tuotteet[i];
     const kate = laskeKate(tuote.ostohinta, tuote.myyntihinta);
-    console.log(`Tuote: ${tuote.tuotenimi}`);
-    console.log(`Kate: ${kate.toFixed(2)}%`);
-    console.log(`Toimittajan linkki: ${tuote.toimittajanLinkki}`);
-    console.log("----------------------------------------");
+    // console.log(`Tuote: ${tuote.tuotenimi}`);
+    // console.log(`Kate: ${kate.toFixed(2)}%`);
+    // console.log(`Toimittajan linkki: ${tuote.toimittajanLinkki}`);
+    // console.log("----------------------------------------");
     tuote.kate = kate.toFixed(2);
     console.log(tuote);
   }
+let x = "";
+  //JavaScript-funktio haeAutot alkaa...
+  function haeTuottet() {
+    //x-muuttujaan rakennetaan HTML-koodisto, jossa sekä taulukon ohjaustägejä, että tuotteet-tiedossa olevaa dataa
+    x ="<table><tr><th>Tuotekoodi</th><th>Nimi</th><th>Ostohinta</th><th>Myyntihinta</th><th>Kate-%</th></tr>"; //taulukon aloitus+otsikot
+      for (let i = 0; i < tuotteet.length; i++) {
+      //käsitellään kaikki JavaObjektissa tuotteet olevat rivit ja lisätään taulukkokoodit kunkin sarakkeen ympärille
+      x += "<tr><td>" + tuotteet[i].tuotekoodi + "</td>";
+      x += "<td>" + tuotteet[i].tuotenimi + "</td>";
+      x += "<td>" + tuotteet[i].ostohinta + "</td>";
+      x += "<td>" + tuotteet[i].myyntihinta + "</td>";
+      x += "<td>" + tuotteet[i].kate + "</td></tr>";
+    }
+    x += "</table>"; //taulukon lopetus
+    //sijoitetaan silmukassa luotu taulukko HTML-sivun kohtaan, jossa on merkintä id="autot"
+
+    document.getElementById("tbl").innerHTML = x;
+  } //JavaScript-funktio haeTuottet loppu...
+  haeTuottet(); //kutsuu funktiota
+
+
+  //<a href='http://" + myObj.cars[i].link + "' target=blank>" + myObj.cars[i].link + "</a>
