9e9785aea658919d29e3fadb87a9c1845eea2066
AndreyGOIT <ae.borga@gmail.com>
2025-01-21 09:53:12 +0200

    first commit: basic files and data

---
+++ script.js
@@ -0,0 +1,52 @@
+//JSON-dataa
+const tuotteet = [
+    {
+      "tuotekoodi": "T001",
+      "tuotenimi": "Kahvi",
+      "ostohinta": 2.5,
+      "myyntihinta": 5.0,
+      "toimittajanLinkki": "http://kahvitoimittaja.com"
+    },
+    {
+      "tuotekoodi": "T002",
+      "tuotenimi": "Tee",
+      "ostohinta": 1.5,
+      "myyntihinta": 3.5,
+      "toimittajanLinkki": "http://teetoimittaja.com"
+    },
+    {
+      "tuotekoodi": "T003",
+      "tuotenimi": "Sokeri",
+      "ostohinta": 0.8,
+      "myyntihinta": 2.0,
+      "toimittajanLinkki": "http://sokeritoimittaja.com"
+    },
+    {
+      "tuotekoodi": "T004",
+      "tuotenimi": "Kaakao",
+      "ostohinta": 3.0,
+      "myyntihinta": 6.5,
+      "toimittajanLinkki": "http://kaakaotoimittaja.com"
+    },
+    {
+      "tuotekoodi": "T005",
+      "tuotenimi": "Maito",
+      "ostohinta": 0.9,
+      "myyntihinta": 1.8,
+      "toimittajanLinkki": "http://maitotoimittaja.com"
+    },
+    {
+      "tuotekoodi": "T006",
+      "tuotenimi": "Juusto",
+      "ostohinta": 2.0,
+      "myyntihinta": 4.5,
+      "toimittajanLinkki": "http://maitotoimittaja.com"
+    },
+    {
+      "tuotekoodi": "T007",
+      "tuotenimi": "Keksi",
+      "ostohinta": 1.0,
+      "myyntihinta": 2.5,
+      "toimittajanLinkki": "http://leipomotoimittaja.com"
+    }
+  ];
