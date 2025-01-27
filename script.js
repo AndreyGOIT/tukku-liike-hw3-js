e5701cdc02b3695a8f4db227bd2a4aae2b9f7084
AndreyGOIT <ae.borga@gmail.com>
2025-01-27 15:29:27 +0200

    add btns to index

--- script.js
+++ script.js
@@ -1,56 +1,61 @@
-/*--------------------- JSON-dataa ---------------------*/
-const tuotteet = [
-    {
-      "tuotekoodi": "T001",
-      "tuotenimi": "Kahvi",
-      "ostohinta": 2.5,
-      "myyntihinta": 5.0,
-      "toimittajanLinkki": "http://kahvitoimittaja.com"
-    },
-    {
-      "tuotekoodi": "T002",
-      "tuotenimi": "Tee",
-      "ostohinta": 1.5,
-      "myyntihinta": 3.5,
-      "toimittajanLinkki": "http://teetoimittaja.com"
-    },
-    {
-      "tuotekoodi": "T003",
-      "tuotenimi": "Sokeri",
-      "ostohinta": 0.8,
-      "myyntihinta": 2.0,
-      "toimittajanLinkki": "http://sokeritoimittaja.com"
-    },
-    {
-      "tuotekoodi": "T004",
-      "tuotenimi": "Kaakao",
-      "ostohinta": 3.0,
-      "myyntihinta": 6.5,
-      "toimittajanLinkki": "http://kaakaotoimittaja.com"
-    },
-    {
-      "tuotekoodi": "T005",
-      "tuotenimi": "Maito",
-      "ostohinta": 0.9,
-      "myyntihinta": 1.8,
-      "toimittajanLinkki": "http://maitotoimittaja.com"
-    },
-    {
-      "tuotekoodi": "T006",
-      "tuotenimi": "Juusto",
-      "ostohinta": 2.0,
-      "myyntihinta": 4.5,
-      "toimittajanLinkki": "http://maitotoimittaja.com"
-    },
-    {
-      "tuotekoodi": "T007",
-      "tuotenimi": "Keksi",
-      "ostohinta": 1.0,
-      "myyntihinta": 2.5,
-      "toimittajanLinkki": "http://leipomotoimittaja.com"
-    }
-  ];
-  /*------------------------- KATE:en käsittely -------------------------------*/
+/*----------------- JSON-dataa -------------*/
+const jsonData = `
+[
+  {
+    "tuotekoodi": "T001",
+    "tuotenimi": "Kahvi",
+    "ostohinta": 2.5,
+    "myyntihinta": 5.0,
+    "toimittajanLinkki": "http://kahvitoimittaja.com"
+  },
+  {
+    "tuotekoodi": "T002",
+    "tuotenimi": "Tee",
+    "ostohinta": 1.5,
+    "myyntihinta": 3.5,
+    "toimittajanLinkki": "http://teetoimittaja.com"
+  },
+  {
+    "tuotekoodi": "T003",
+    "tuotenimi": "Sokeri",
+    "ostohinta": 0.8,
+    "myyntihinta": 2.0,
+    "toimittajanLinkki": "http://sokeritoimittaja.com"
+  },
+  {
+    "tuotekoodi": "T004",
+    "tuotenimi": "Kaakao",
+    "ostohinta": 3.0,
+    "myyntihinta": 6.5,
+    "toimittajanLinkki": "http://kaakaotoimittaja.com"
+  },
+  {
+    "tuotekoodi": "T005",
+    "tuotenimi": "Maito",
+    "ostohinta": 0.9,
+    "myyntihinta": 1.8,
+    "toimittajanLinkki": "http://maitotoimittaja.com"
+  },
+  {
+    "tuotekoodi": "T006",
+    "tuotenimi": "Juusto",
+    "ostohinta": 2.0,
+    "myyntihinta": 4.5,
+    "toimittajanLinkki": "http://maitotoimittaja.com"
+  },
+  {
+    "tuotekoodi": "T007",
+    "tuotenimi": "Keksi",
+    "ostohinta": 1.0,
+    "myyntihinta": 2.5,
+    "toimittajanLinkki": "http://leipomotoimittaja.com"
+  }
+]
+`;
+
+// JSON-merkkijonon jäsentäminen JavaScript-objekteiksi
+const tuotteet = JSON.parse(jsonData);
+  /*------------- KATE:en käsittely --------------------*/
   //function jokaa laskee kate
   function laskeKate(ostohinta, myyntihinta) {
     const kate = ((myyntihinta - ostohinta) / ostohinta) * 100;
@@ -60,17 +65,13 @@ const tuotteet = [
   for (let i = 0; i < tuotteet.length; i++) {
     const tuote = tuotteet[i];
     const kate = laskeKate(tuote.ostohinta, tuote.myyntihinta);
-    // console.log(`Tuote: ${tuote.tuotenimi}`);
-    // console.log(`Kate: ${kate.toFixed(2)}%`);
-    // console.log(`Toimittajan linkki: ${tuote.toimittajanLinkki}`);
-    // console.log("----------------------------------------");
+    
     tuote.kate = kate.toFixed(2);
-    //console.log(tuote);
   }
 
-/*----------------------- TUOTTEET taulukko--------------------------------*/
+/*------------------ TUOTTEET taulukko---------------------*/
 let x = "";
-  //JavaScript-funktio haeAutot alkaa...
+  //JavaScript-funktio haeTuottet alkaa...
   function haeTuottet() {
     //x-muuttujaan rakennetaan HTML-koodisto, jossa sekä taulukon ohjaustägejä, että tuotteet-tiedossa olevaa dataa
     x ="<table><tr><th>Tuotekoodi</th><th>Nimi</th><th>Ostohinta</th><th>Myyntihinta</th><th>Kate-%</th></tr>"; //taulukon aloitus+otsikot
@@ -87,9 +88,9 @@ let x = "";
 
     document.getElementById("tbl").innerHTML = x;
   } //JavaScript-funktio haeTuottet loppu...
-  haeTuottet(); //kutsuu funktiota
+  //haeTuottet(); //kutsuu funktiota
 
-/*----------------------- TOIMITTAJAN linkit --------------------------------*/
+/*---------------- TOIMITTAJAN linkit ------------------*/
 //versio 1
 /*let toimLinkkiArray = []; //taulukko, johon lisätään toimittajien linkit
 for (let i = 0; i < tuotteet.length; i++) {
@@ -153,4 +154,4 @@ function haeToimittajat() {
      });
   }
   
-  haeToimittajat();
+  //haeToimittajat();
